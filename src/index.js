407618ca00a26a682fda2c9afdd6a24a824e0920
Daniel <dselasea@gmail.com>
2023-02-22 23:56:52 +0000

    Add class, add event listeners, modify button style

--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
 import './style.css';
+import Todos from './todo';
 
-const tasks = [
-  {
-    description: 'create Todo List',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'do chores',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'sweep compound',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'wash car',
-    completed: false,
-    index: 4,
-  },
-];
-
+let tasks = [];
+const clearTodos = document.querySelector('.clear');
 const taskList = document.querySelector('.tasks-list');
+const todoForm = document.querySelector('.form');
+const inputTodos = document.querySelector('.form .input');
+
+todoForm.addEventListener('submit', (e) => {
+  let id = tasks.length + 1;
+  const todos = new Todos(id, inputTodos.value, false);
+  if(e.key = 'Enter'){
+    if(inputTodos.value){
+      tasks.push(todos);
+      clearInput();
+      todoList(tasks);
+    }
+  }
+  e.preventDefault();
+})
 
 const todoList = (tasks) => {
   let htmlList = '';
   for (let i = 0; i < tasks.length; i += 1) {
-    htmlList += `<li id=${tasks[i].index}><label class="check-container"><input type="checkbox" >${tasks[i].description}</label><i class="fa fa-ellipsis-v"></i></li>`;
+    htmlList += `<li id=${tasks[i].index}><label class="check-container"><input type="checkbox" >${tasks[i].description}</label><i class="fa fa-ellipsis-v fav"></i></li>`;
   }
 
-  taskList.innerHTML = (htmlList);
+  taskList.innerHTML = htmlList;
 };
 
-todoList(tasks);
+// Clear Todos
+const clearInput = () => {
+  inputTodos.value = '';
+}
+
+// Clear all todos 
+clearTodos.addEventListener('click', (e) => {
+    console.log(e.target.className);
+    if(e.target.className === 'clear'){
+      taskList.innerHTML = '';
+      tasks = [];
+    }
+})
+
