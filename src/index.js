b11f948c73f07dcbd2e477ca0eabec1672641570
Daniel <dselasea@gmail.com>
2023-02-22 02:02:08 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import _ from 'lodash';
 import './style.css';
 
 const tasks = [
   {
     description: 'create Todo List',
     completed: false,
-    index: 1
+    index: 1,
   },
   {
     description: 'do chores',
     completed: true,
-    index: 2
+    index: 2,
   },
   {
     description: 'sweep compound',
     completed: false,
-    index: 3
+    index: 3,
   },
   {
     description: 'wash car',
     completed: false,
-    index: 4
+    index: 4,
   },
 ];
 
@@ -28,8 +27,8 @@ const taskList = document.querySelector('.tasks-list');
 
 const todoList = (tasks) => {
   let htmlList = '';
-  for(let i = 0; i < tasks.length; i += 1){
-    htmlList += `<li id=${tasks[i].index}><label class="check-container"><input type="checkbox" >${tasks[i].description}</label><i class="fa fa-ellipsis-v"></i></li>`
+  for (let i = 0; i < tasks.length; i += 1) {
+    htmlList += `<li id=${tasks[i].index}><label class="check-container"><input type="checkbox" >${tasks[i].description}</label><i class="fa fa-ellipsis-v"></i></li>`;
   }
 
   taskList.innerHTML = (htmlList);
