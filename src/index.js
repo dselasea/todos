c3053b39d9b049fcec1bc0749e9d31d676f03c54
Daniel <dselasea@gmail.com>
2023-02-22 01:18:19 +0000

    Add content to index.html, add styles, add list dynamically

--- src/index.js
+++ src/index.js
@@ -1,2 +1,41 @@
 import _ from 'lodash';
 import './style.css';
+
+const tasks = [
+  {
+    description: 'create Todo List',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'do chores',
+    completed: true,
+    index: 2
+  },
+  {
+    description: 'sweep compound',
+    completed: false,
+    index: 3
+  },
+  {
+    description: 'wash car',
+    completed: false,
+    index: 4
+  },
+];
+
+const taskList = document.querySelector('.tasks-list');
+
+const todoList = (tasks) => {
+  // tasks.map((task) => {
+  //   console.log(task.completed, task.index, task.description);
+  // })
+  let htmlList = '';
+  for(let i = 0; i < tasks.length; i += 1){
+    htmlList += `<li id=${tasks[i].index}><label class="check-container"><input type="checkbox" checked=${tasks[i].completed} >${tasks[i].description}</label></li>`
+  }
+
+  taskList.innerHTML = (htmlList);
+};
+
+todoList(tasks);
