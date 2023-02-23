aeb0f5c913537babfb2af7256738028142c29370
Daniel <dselasea@gmail.com>
2023-02-23 00:38:55 +0000

    Add clear function and delete todo event handler

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import Todos from './todo';
 
 let tasks = [];
 const clearTodos = document.querySelector('.clear');
+// const deleteTodos = document.querySelector('.trash');
 const taskList = document.querySelector('.tasks-list');
 const todoForm = document.querySelector('.form');
 const inputTodos = document.querySelector('.form .input');
 
+// Enter to submit
 todoForm.addEventListener('submit', (e) => {
   let id = tasks.length + 1;
   const todos = new Todos(id, inputTodos.value, false);
@@ -23,20 +25,28 @@ todoForm.addEventListener('submit', (e) => {
 const todoList = (tasks) => {
   let htmlList = '';
   for (let i = 0; i < tasks.length; i += 1) {
-    htmlList += `<li id=${tasks[i].index}><label class="check-container"><input type="checkbox" >${tasks[i].description}</label><i class="fa fa-ellipsis-v fav"></i></li>`;
+    htmlList += `<li><label class="check-container"><input type="checkbox" >${tasks[i].description}</label><i class="fa fa-ellipsis-v fav"></i><i class="fa fa-trash-o trash" id=${tasks[i].index}></i></li>`;
   }
 
   taskList.innerHTML = htmlList;
 };
 
-// Clear Todos
+// Delete Todos 
+todoForm.addEventListener('click', (e) => {
+  if(e.target.id){
+    e.target.parentElement.remove();
+    console.log(tasks.splice(e.target.id, 1));
+    // return tasks.splice(e.target.id, 1);
+  }
+})
+
+// Clear input value
 const clearInput = () => {
   inputTodos.value = '';
 }
 
 // Clear all todos 
 clearTodos.addEventListener('click', (e) => {
-    console.log(e.target.className);
     if(e.target.className === 'clear'){
       taskList.innerHTML = '';
       tasks = [];
