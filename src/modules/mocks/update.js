84358e7b5f6caa428ffbc21deb7615e6fc7424d6
Daniel <dselasea@gmail.com>
2023-03-02 16:01:12 +0000

    add clearall, delete, update and updateItems.js

--- src/modules/mocks/update.js
+++ src/modules/mocks/update.js
@@ -1,19 +1,4 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable no-unused-expressions */
-import updateItems from './updateItems.js';
-
-const update = (index) => {
-  let todos = [];
-
-  todos = JSON.parse(localStorage.getItem('todoData')) || [];
-
-  const update = todos.filter((todo) => {
-    todo.index === index && (item.description = "Pray");
-    return todo;
-  });
-  localStorage.setItem('localItem', JSON.stringify(update));
-
-  updateItems();
-};
-
-export default update;
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
